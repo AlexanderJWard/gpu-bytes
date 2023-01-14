fdb3c1d0171cb6ca18c899fed8edeb882e6e262a
AlexanderJWard <alexjamesward@hotmail.com>
2023-01-14 21:52:59 +0000

    Remove Whitenoise

--- gpubytes/settings.py
+++ gpubytes/settings.py
@@ -74,7 +74,6 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
