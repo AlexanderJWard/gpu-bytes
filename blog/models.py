c5b53e979e76643ea0d2c1e7e03792972df5ada8
AlexanderJWard <alexjamesward@hotmail.com>
2023-01-14 15:48:41 +0000

    Finish adding comments and likes to post detail. Add alert messages to base.html

--- blog/models.py
+++ blog/models.py
@@ -27,6 +27,9 @@ class Post(models.Model):
     def number_of_likes(self):
         return self.likes.count()
 
+
+
+
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
